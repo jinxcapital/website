c4b827c9630499ffea9bc2e80c08d7d9521d2ef3
0xpowder <0xpowder@protonmail.com>
2021-12-12 20:41:42 +0100

    :recycle: Refactored layout a little & data flow

--- src/components/_pages/Landing/index.tsx
+++ src/components/_pages/Landing/index.tsx
@@ -1,10 +1,35 @@
 import BitcoinExchangeNetflow from 'components/BitcoinExchangeNetflow';
 import Charts from 'components/Charts';
 import Layout from 'components/Layout';
+import { useCoins } from 'data/coins/hooks';
+import { useMemo } from 'react';
+import { formatCurrency, formatPercentage } from 'utils/formatters';
 
 const Landing = () => {
+  const { bitcoin } = useCoins();
+
+  const title = useMemo(() => {
+    if (bitcoin) {
+      return `Jinx Capital - Bitcoin: ${formatCurrency(bitcoin.price)}`;
+    }
+
+    return 'Jinx Capital';
+  }, [bitcoin]);
+
+  const description = useMemo(() => {
+    if (bitcoin) {
+      return `Jinx Capital is a fictive cryptonative investment fund of @0xpowder. Btw the price of Bitcoin changed with ${formatPercentage(
+        bitcoin.percentageChange24h / 100,
+      )} in the last 24h and is currently sitting at ${formatCurrency(
+        bitcoin.price,
+      )}!`;
+    }
+
+    return 'Jinx Capital is a fictive cryptonative investment fund of @0xpowder.';
+  }, [bitcoin]);
+
   return (
-    <Layout>
+    <Layout title={title} description={description}>
       <BitcoinExchangeNetflow />
       <Charts />
     </Layout>
