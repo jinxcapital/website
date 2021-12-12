3ae5f5077d584d229fa7aa67b8378d177cf25814
jinxcapital <jinxcapital@protonmail.com>
2021-12-12 20:57:43 +0100

    :construction: Placeholder pages

---
+++ src/components/_pages/API/index.tsx
@@ -0,0 +1,11 @@
+import Layout from 'components/Layout';
+
+const API = () => {
+  return (
+    <Layout title="API - Jinx Capital">
+      <p>https://api.jinx.capital</p>
+    </Layout>
+  );
+};
+
+export default API;
---
+++ src/components/_pages/Charts/index.tsx
@@ -0,0 +1,11 @@
+import Layout from 'components/Layout';
+
+const Charts = () => {
+  return (
+    <Layout title="Charts - Jinx Capital">
+      <p>Charts</p>
+    </Layout>
+  );
+};
+
+export default Charts;
