261a5ddc1f7539d576750a1bd25e44026b907b2c
0xpowder <0xpowder@protonmail.com>
2021-12-12 21:16:47 +0100

    :sparkles: /api-docs page

---
+++ src/components/_pages/APIDocs/index.tsx
@@ -0,0 +1,16 @@
+import Layout from 'components/Layout';
+
+const APIDocs = () => {
+  return (
+    <Layout title="API docs - Jinx Capital">
+      <h1>API Docs</h1>
+      <p>
+        <a href="https://api.jinx.capital" target="_blank" rel="noreferrer">
+          https://api.jinx.capital
+        </a>
+      </p>
+    </Layout>
+  );
+};
+
+export default APIDocs;
