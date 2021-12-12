0ed8932c58029432d44da05402ad076f54693261
jinxcapital <jinxcapital@protonmail.com>
2021-12-12 19:12:45 +0100

    :lipstick: Footer ref links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,6 +3,26 @@ import styles from './styles.module.css';
 const Footer = () => {
   return (
     <footer className={styles.container}>
+      <ul>
+        <li>
+          <a
+            href="https://ftx.com/referrals#a=jinxcapital"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Trade with FTX
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://app.coinmarketman.com/exchanges/?rf=38514"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Track with CoinMarketMan
+          </a>
+        </li>
+      </ul>
       &copy; {new Date().getFullYear()} Jinx Capital
     </footer>
   );
