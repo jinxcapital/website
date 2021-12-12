c75f2a1ffbdc6276637fb35a83e6c526f7778279
0xpowder <0xpowder@protonmail.com>
2021-12-12 12:26:56 +0100

    :recycle: Extract coins fetching logic to context

--- src/components/_pages/Landing/index.tsx
+++ src/components/_pages/Landing/index.tsx
@@ -1,15 +1,12 @@
 import BitcoinExchangeNetflow from 'components/BitcoinExchangeNetflow';
 import Charts from 'components/Charts';
 import Layout from 'components/Layout';
-import { useCoins } from 'data/coins/hooks';
 
 const Landing = () => {
-  const { coins } = useCoins();
-
   return (
     <Layout>
       <BitcoinExchangeNetflow />
-      <Charts coins={coins} />
+      <Charts />
     </Layout>
   );
 };
