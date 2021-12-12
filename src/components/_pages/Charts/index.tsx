d363c54f20909f76e478ff2eaec746d1d38b0a05
jinxcapital <jinxcapital@protonmail.com>
2021-12-12 21:31:46 +0100

    :construction: Temp content

--- src/components/_pages/Charts/index.tsx
+++ src/components/_pages/Charts/index.tsx
@@ -1,9 +1,34 @@
 import Layout from 'components/Layout';
+import { useCoins } from 'data/coins/hooks';
+
+import styles from './styles.module.css';
 
 const Charts = () => {
+  const { bitcoin } = useCoins();
   return (
     <Layout title="Charts - Jinx Capital">
-      <p>Charts</p>
+      <h1>Charts</h1>
+      <div className={styles.charts}>
+        <div className={styles.link}>
+          <p>
+            <a
+              href={`${process.env.NEXT_PUBLIC_API_URL}/chart/btc:usd.jpg`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {`${process.env.NEXT_PUBLIC_API_URL}/chart/btc:usd.jpg`}
+            </a>
+          </p>
+        </div>
+        <p>
+          <img
+            src={`${process.env.NEXT_PUBLIC_API_URL}/chart/btc:usd.jpg?${bitcoin?.price}`}
+            alt={`${process.env.NEXT_PUBLIC_API_URL}/chart/btc:usd.jpg`}
+            loading="lazy"
+            className={styles.img}
+          />
+        </p>
+      </div>
     </Layout>
   );
 };
