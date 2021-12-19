f904c93ea9f81ab7b7e3324f914aa825f97e15a8
jinxcapital <jinxcapital@protonmail.com>
2021-12-19 20:47:49 +0100

    :bug: Data might not be available

--- src/data/funding/hooks.ts
+++ src/data/funding/hooks.ts
@@ -17,6 +17,10 @@ export const useFunding = (refreshInterval = 30000) => {
   const funding = useMemo(() => {
     return coins
       .map((coin) => {
+        if (!responseFunding?.data?.data) {
+          return null;
+        }
+
         const rates = Object.entries(responseFunding?.data?.data)
           .find(([coinId]) => coinId === coin.id)
           ?.pop();
