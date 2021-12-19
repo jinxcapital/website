aae5644c72b658a0ce50f5a2ee7b63fc738ce066
jinxcapital <jinxcapital@protonmail.com>
2021-12-19 11:55:18 +0100

    :recycle: Only show when data loaded

--- src/components/CoreData/index.tsx
+++ src/components/CoreData/index.tsx
@@ -16,69 +16,73 @@ const CoreData = () => {
 
   return (
     <ul className={styles.container}>
-      <li className={styles.entry}>
-        <strong className={styles.title}>Bitcoin exchange netflow</strong>{' '}
-        <ul>
-          <li>
-            <strong>24 hours</strong>{' '}
-            <span>
-              {exchangeNetflow?.change?.day
-                ? formatBigNumber(exchangeNetflow.change.day)
-                : '--'}{' '}
-              BTC
-            </span>
-          </li>
-          <li>
-            <strong>7 days</strong>{' '}
-            <span>
-              {exchangeNetflow?.change?.week
-                ? formatBigNumber(exchangeNetflow.change.week)
-                : '--'}{' '}
-              BTC
-            </span>
-          </li>
-          <li>
-            <strong>30 days</strong>{' '}
-            <span>
-              {exchangeNetflow?.change?.month
-                ? formatBigNumber(exchangeNetflow.change.month)
-                : '--'}{' '}
-              BTC
-            </span>
-          </li>
-        </ul>
-      </li>
-      <li className={styles.entry}>
-        <strong className={styles.title}>Leverage in the market</strong>{' '}
-        <ul>
-          <li>
-            <strong>Open interest</strong>{' '}
-            <span>
-              {leverage?.openInterest
-                ? formatCurrenyCompact(leverage?.openInterest)
-                : '--'}
-            </span>
-          </li>
-          <li>
-            <strong>24H liquidations</strong>{' '}
-            <span>
-              {leverage?.liquidations24h
-                ? formatCurrenyCompact(leverage?.liquidations24h)
-                : '--'}
-            </span>
-          </li>
-          <li>
-            <strong>L/S ratio</strong>{' '}
-            <span>
-              {leverage?.longRate && leverage?.shortRate
-                ? `${percentageFormatter.format(
-                    leverage?.longRate / 100,
-                  )} ${percentageFormatter.format(leverage?.shortRate / 100)}`
-                : '--'}
-            </span>
-          </li>
-        </ul>
-      </li>
+      {exchangeNetflow && (
+        <li className={styles.entry}>
+          <strong className={styles.title}>Bitcoin exchange netflow</strong>{' '}
+          <ul>
+            <li>
+              <strong>24 hours</strong>{' '}
+              <span>
+                {exchangeNetflow?.change?.day
+                  ? formatBigNumber(exchangeNetflow.change.day)
+                  : '--'}{' '}
+                BTC
+              </span>
+            </li>
+            <li>
+              <strong>7 days</strong>{' '}
+              <span>
+                {exchangeNetflow?.change?.week
+                  ? formatBigNumber(exchangeNetflow.change.week)
+                  : '--'}{' '}
+                BTC
+              </span>
+            </li>
+            <li>
+              <strong>30 days</strong>{' '}
+              <span>
+                {exchangeNetflow?.change?.month
+                  ? formatBigNumber(exchangeNetflow.change.month)
+                  : '--'}{' '}
+                BTC
+              </span>
+            </li>
+          </ul>
+        </li>
+      )}
+      {leverage && (
+        <li className={styles.entry}>
+          <strong className={styles.title}>Leverage in the market</strong>{' '}
+          <ul>
+            <li>
+              <strong>Open interest</strong>{' '}
+              <span>
+                {leverage?.openInterest
+                  ? formatCurrenyCompact(leverage?.openInterest)
+                  : '--'}
+              </span>
+            </li>
+            <li>
+              <strong>24H liquidations</strong>{' '}
+              <span>
+                {leverage?.liquidations24h
+                  ? formatCurrenyCompact(leverage?.liquidations24h)
+                  : '--'}
+              </span>
+            </li>
+            <li>
+              <strong>L/S ratio</strong>{' '}
+              <span>
+                {leverage?.longRate && leverage?.shortRate
+                  ? `${percentageFormatter.format(
+                      leverage?.longRate / 100,
+                    )} ${percentageFormatter.format(leverage?.shortRate / 100)}`
+                  : '--'}
+              </span>
+            </li>
+          </ul>
+        </li>
+      )}
     </ul>
   );
 };
