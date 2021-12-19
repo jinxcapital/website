2d05cc64b2e227c9bbcd046e911245051ab05c0a
jinxcapital <jinxcapital@protonmail.com>
2021-12-19 20:03:47 +0100

    :lipstick: Changed core data a little + show multiple funding rates

--- src/components/CoreData/index.tsx
+++ src/components/CoreData/index.tsx
@@ -1,3 +1,4 @@
+import { useCoins } from 'data/coins/hooks';
 import { useExchangeNetflow } from 'data/exchange-netflow/hooks';
 import { useFunding } from 'data/funding/hooks';
 import { useLeverage } from 'data/leverage/hooks';
@@ -18,6 +19,7 @@ const fundingFormatter = new Intl.NumberFormat('en-US', {
 });
 
 const CoreData = () => {
+  const { coins } = useCoins();
   const { exchangeNetflow } = useExchangeNetflow('bitcoin');
   const { leverage } = useLeverage();
   const { funding } = useFunding();
@@ -29,7 +31,7 @@ const CoreData = () => {
           <strong className={styles.title}>Bitcoin exchange netflow</strong>
           <ul>
             <li>
-              <strong>24 hours</strong>{' '}
+              <strong>24H</strong>{' '}
               <span>
                 {exchangeNetflow?.change?.day
                   ? formatBigNumber(exchangeNetflow.change.day)
@@ -38,7 +40,7 @@ const CoreData = () => {
               </span>
             </li>
             <li>
-              <strong>7 days</strong>{' '}
+              <strong>7D</strong>{' '}
               <span>
                 {exchangeNetflow?.change?.week
                   ? formatBigNumber(exchangeNetflow.change.week)
@@ -47,7 +49,7 @@ const CoreData = () => {
               </span>
             </li>
             <li>
-              <strong>30 days</strong>{' '}
+              <strong>30D</strong>{' '}
               <span>
                 {exchangeNetflow?.change?.month
                   ? formatBigNumber(exchangeNetflow.change.month)
@@ -79,7 +81,7 @@ const CoreData = () => {
               </span>
             </li>
             <li>
-              <strong>L/S ratio</strong>{' '}
+              <strong>LSR</strong>{' '}
               <span>
                 {leverage?.longRate && leverage?.shortRate
                   ? `${percentageFormatter.format(
@@ -91,35 +93,39 @@ const CoreData = () => {
           </ul>
         </li>
       )}
-      {funding && (
-        <li className={styles.entry}>
-          <strong className={styles.title}>Aggregated funding rates</strong>
-          <ul>
-            {Object.entries(funding)
-              .slice(0, 3)
-              .map(([coin, data]) => (
-                <li key={`aggr-funding:${coin}`}>
-                  <strong>
-                    {coin.charAt(0).toUpperCase() + coin.slice(1)}
-                  </strong>{' '}
-                  <span>
-                    {fundingFormatter.format(
-                      (data as Array<{ rate: number }>).reduce(function (
-                        sum,
-                        value,
-                      ) {
-                        return sum + value.rate;
-                      },
-                      0) /
-                        (data as Array<unknown>).length /
-                        100,
-                    )}
-                  </span>
-                </li>
-              ))}
-          </ul>
-        </li>
-      )}
+      {funding &&
+        Array.from(
+          Array(Math.ceil(Object.keys(funding).length / 3)).keys(),
+        ).map((index) => (
+          <li className={styles.entry} key={`aggr-funding.list:${index}`}>
+            <strong className={styles.title}>Aggregated funding rates</strong>
+            <ul>
+              {Object.entries(funding)
+                .slice(index * 3, index * 3 + 3)
+                .map(([coinId, data]) => {
+                  const coin = coins.find((coin) => coin.id === coinId);
+                  const rates = (data as Array<{ rate: number }>).filter(
+                    (value) => !isNaN(value.rate),
+                  );
+
+                  return (
+                    <li key={`aggr-funding:${coin}`}>
+                      <strong>{coin?.name}</strong>{' '}
+                      <span>
+                        {fundingFormatter.format(
+                          rates.reduce((sum, value) => {
+                            return sum + value.rate;
+                          }, 0) /
+                            rates.length /
+                            100,
+                        )}
+                      </span>
+                    </li>
+                  );
+                })}
+            </ul>
+          </li>
+        ))}
     </ul>
   );
 };
