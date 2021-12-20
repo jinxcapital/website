0e08d2db88991b52e17cef67cbc8ec093d682a8e
jinxcapital <jinxcapital@protonmail.com>
2021-12-20 22:22:33 +0100

    :sparkles: Funding cards scroller

--- src/components/CoreData/index.tsx
+++ src/components/CoreData/index.tsx
@@ -2,6 +2,7 @@ import TextSpinner from 'components/TextSpinner';
 import { useExchangeNetflow } from 'data/exchange-netflow/hooks';
 import { useFunding } from 'data/funding/hooks';
 import { useLeverage } from 'data/leverage/hooks';
+import { useCallback } from 'react';
 import { formatBigNumber, formatCurrenyCompact } from 'utils/formatters';
 
 import styles from './styles.module.css';
@@ -24,119 +25,141 @@ const CoreData = () => {
   const { leverage } = useLeverage();
   const { funding } = useFunding();
 
+  const CoreData = useCallback(
+    () => (
+      <ul className={styles.coreData}>
+        <li className={styles.entry}>
+          <div>
+            <strong className={styles.title}>Bitcoin exchange netflow</strong>
+            <ul>
+              <li>
+                {exchangeNetflow?.change?.day ? (
+                  <>
+                    <strong>24H</strong>{' '}
+                    <span>
+                      {formatBigNumber(exchangeNetflow.change.day)} BTC
+                    </span>
+                  </>
+                ) : (
+                  <TextSpinner />
+                )}
+              </li>
+              <li>
+                {exchangeNetflow?.change?.week ? (
+                  <>
+                    <strong>7D</strong>{' '}
+                    <span>
+                      {formatBigNumber(exchangeNetflow.change.week)} BTC
+                    </span>
+                  </>
+                ) : (
+                  <TextSpinner />
+                )}
+              </li>
+              <li>
+                {exchangeNetflow?.change?.month ? (
+                  <>
+                    <strong>30D</strong>{' '}
+                    <span>
+                      {formatBigNumber(exchangeNetflow.change.month)} BTC
+                    </span>
+                  </>
+                ) : (
+                  <TextSpinner />
+                )}
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li className={styles.entry}>
+          <div>
+            <strong className={styles.title}>Leverage in the market</strong>
+            <ul>
+              <li>
+                {leverage?.openInterest ? (
+                  <>
+                    <strong>Open interest</strong>{' '}
+                    <span>{formatCurrenyCompact(leverage?.openInterest)}</span>
+                  </>
+                ) : (
+                  <TextSpinner />
+                )}
+              </li>
+              <li>
+                {leverage?.liquidations24h ? (
+                  <>
+                    <strong>24H liquidations</strong>{' '}
+                    <span>
+                      {formatCurrenyCompact(leverage?.liquidations24h)}
+                    </span>
+                  </>
+                ) : (
+                  <TextSpinner />
+                )}
+              </li>
+              <li>
+                {leverage?.longRate && leverage?.shortRate ? (
+                  <>
+                    <strong>LSR</strong>{' '}
+                    <span>
+                      {percentageFormatter.format(leverage?.longRate / 100)}{' '}
+                      {percentageFormatter.format(leverage?.shortRate / 100)}
+                    </span>
+                  </>
+                ) : (
+                  <TextSpinner />
+                )}
+              </li>
+            </ul>
+          </div>
+        </li>
+        {funding.length &&
+          Array.from(Array(Math.floor(7)).keys()).map((index) => (
+            <li className={styles.entry} key={`aggr-funding.list:${index}`}>
+              <div>
+                <strong className={styles.title}>
+                  Aggregated funding rates
+                </strong>
+                <ul>
+                  {funding.slice(index * 3, index * 3 + 3).map((funding) => {
+                    const rates = (
+                      funding?.rates as Array<{ rate: number }>
+                    ).filter((value) => !isNaN(value.rate));
+
+                    const rate =
+                      rates.reduce((sum, value) => {
+                        return sum + value.rate;
+                      }, 0) /
+                      rates.length /
+                      100;
+
+                    return (
+                      <li key={`aggr-funding:${funding?.coin.id}`}>
+                        <strong>{funding?.coin.name}</strong>{' '}
+                        <span>{fundingFormatter.format(rate)}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </li>
+          ))}
+      </ul>
+    ),
+    [funding, leverage, exchangeNetflow],
+  );
+
   return (
-    <ul className={styles.container}>
-      <li className={styles.entry}>
-        <div>
-          <strong className={styles.title}>Bitcoin exchange netflow</strong>
-          <ul>
-            <li>
-              {exchangeNetflow?.change?.day ? (
-                <>
-                  <strong>24H</strong>{' '}
-                  <span>{formatBigNumber(exchangeNetflow.change.day)} BTC</span>
-                </>
-              ) : (
-                <TextSpinner />
-              )}
-            </li>
-            <li>
-              {exchangeNetflow?.change?.week ? (
-                <>
-                  <strong>7D</strong>{' '}
-                  <span>
-                    {formatBigNumber(exchangeNetflow.change.week)} BTC
-                  </span>
-                </>
-              ) : (
-                <TextSpinner />
-              )}
-            </li>
-            <li>
-              {exchangeNetflow?.change?.month ? (
-                <>
-                  <strong>30D</strong>{' '}
-                  <span>
-                    {formatBigNumber(exchangeNetflow.change.month)} BTC
-                  </span>
-                </>
-              ) : (
-                <TextSpinner />
-              )}
-            </li>
-          </ul>
-        </div>
-      </li>
-      <li className={styles.entry}>
-        <div>
-          <strong className={styles.title}>Leverage in the market</strong>
-          <ul>
-            <li>
-              {leverage?.openInterest ? (
-                <>
-                  <strong>Open interest</strong>{' '}
-                  <span>{formatCurrenyCompact(leverage?.openInterest)}</span>
-                </>
-              ) : (
-                <TextSpinner />
-              )}
-            </li>
-            <li>
-              {leverage?.liquidations24h ? (
-                <>
-                  <strong>24H liquidations</strong>{' '}
-                  <span>{formatCurrenyCompact(leverage?.liquidations24h)}</span>
-                </>
-              ) : (
-                <TextSpinner />
-              )}
-            </li>
-            <li>
-              {leverage?.longRate && leverage?.shortRate ? (
-                <>
-                  <strong>LSR</strong>{' '}
-                  <span>
-                    {percentageFormatter.format(leverage?.longRate / 100)}{' '}
-                    {percentageFormatter.format(leverage?.shortRate / 100)}
-                  </span>
-                </>
-              ) : (
-                <TextSpinner />
-              )}
-            </li>
-          </ul>
-        </div>
-      </li>
-      {funding &&
-        Array.from(Array(Math.floor(12)).keys()).map((index) => (
-          <li className={styles.entry} key={`aggr-funding.list:${index}`}>
-            <div>
-              <strong className={styles.title}>Aggregated funding rates</strong>
-              <ul>
-                {funding.slice(index * 3, index * 3 + 3).map((funding) => {
-                  const rates = (
-                    funding?.rates as Array<{ rate: number }>
-                  ).filter((value) => !isNaN(value.rate));
-
-                  const rate =
-                    rates.reduce((sum, value) => {
-                      return sum + value.rate;
-                    }, 0) /
-                    rates.length /
-                    100;
-
-                  return (
-                    <li key={`aggr-funding:${funding?.coin.id}`}>
-                      <strong>{funding?.coin.name}</strong>{' '}
-                      <span>{fundingFormatter.format(rate)}</span>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          </li>
-        ))}
-    </ul>
+    <div className={styles.container}>
+      <div
+        className={`${styles.data} ${
+          exchangeNetflow && leverage && funding ? styles.loaded : ''
+        }`}
+      >
+        <CoreData />
+        <CoreData />
+      </div>
+    </div>
   );
 };
 
